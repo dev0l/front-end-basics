6bf480a3671486f86978a8828f24f61d7c37f823
Marcus <marcus.mobark@gmail.com>
2023-12-14 15:18:53 +0100

    testing dropdown and added notes.txt

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,1 +1,12 @@
 console.log("testing!")
+
+$(document).ready(function () {
+    $('#navbar li').hover(
+    function () {
+        //show submenu
+        $('ul', this).show();
+    }, function () {
+        //hide submenu
+        $('ul', this).hide();
+    });
+});
