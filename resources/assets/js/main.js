db5480348989b65ede2ac06bf7d1cd8e4e58b4f6
Marcus Mobark <marcus.mobark@gmail.com>
2024-01-17 16:38:57 +0100

    formatting, grammar, comments, semantics and added some text to existing topics

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,12 +1,12 @@
-console.log("testing!")
+// console.log("testing!")
 
-$(document).ready(function () {
-    $('#navbar li').hover(
-    function () {
-        //show submenu
-        $('ul', this).show();
-    }, function () {
-        //hide submenu
-        $('ul', this).hide();
-    });
-});
+// $(document).ready(function () {
+//     $('#navbar li').hover(
+//     function () {
+//         //show submenu
+//         $('ul', this).show();
+//     }, function () {
+//         //hide submenu
+//         $('ul', this).hide();
+//     });
+// });
